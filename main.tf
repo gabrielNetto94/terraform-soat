6abe52cdc640955350186afabdfceda6a85e2f7f
gabrielNetto94 <silveiranetto94@gmail.com>
2026-06-04 18:32:36 -0300

    Refactor Terraform configuration by restructuring module paths, removing unused files, and adding new resources for EC2 and S3. Update provider configurations and outputs for better organization and clarity.

--- main.tf
+++ main.tf
@@ -1,14 +1,29 @@
 module "ec2_instance" {
-    source = "./terraform-modules/ec2"
+    source = "./modules/ec2"
 
     instance_type = "t3.nano"
     instance_name = "fiap-ec2-instance"
-    ami_id = data.aws_ami.ubuntu.id
+    ami_id = data.aws_ami.ubuntu.id    
 }
 
 module "s3_bucket" {
-    source = "./terraform-modules/s3"
-
-    bucket_name = "fiap-terraform-state"
-    
+    source = "./modules/s3"
+    bucket_name = "fiap-terraform-state"       
 }
+
+
+# # Configure the AWS Provider
+# provider "aws" {
+#   region                      = "sa-east-1"
+#   access_key                  = "test"
+#   secret_key                  = "test"
+#   skip_credentials_validation = true
+#   skip_metadata_api_check     = true
+#   skip_requesting_account_id  = true
+
+#   endpoints {
+#     s3  = "http://localhost:4566"
+#     ec2 = "http://localhost:4566"
+#     sts = "http://localhost:4566"
+#   }
+# }
