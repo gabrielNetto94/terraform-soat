0046cee524466ea2d8fcd5f34111671a97b1d596
gabrielNetto94 <silveiranetto94@gmail.com>
2026-06-04 14:45:45 -0300

    Remove unnecessary blank lines in main Terraform configuration

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
-
-
-
 module "ec2_instance" {
     source = "./terraform-modules/ec2"
 
     instance_type = "t3.nano"
     instance_name = "fiap-ec2-instance"
     ami_id = data.aws_ami.ubuntu.id
-
 }
 
 module "s3_bucket" {
